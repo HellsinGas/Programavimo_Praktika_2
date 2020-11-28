d0b7ba1df6467fe9db93ef9736b0d7d0c7fb17f1
HellsinGas <65170199+HellsinGas@users.noreply.github.com>
2020-11-28 16:22:00 +0200

    Add project files.

---
+++ Programavimo_Praktika_2/LogedInScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programavimo_Praktika_2
+{
+    public partial class LogedInScreen : Form
+    {
+        public LogedInScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Programavimo_Praktika_2/LoginScreen.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Programavimo_Praktika_2
+{
+    partial class LoginScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernamebox = new System.Windows.Forms.TextBox();
+            this.passbox = new System.Windows.Forms.TextBox();
+            this.loginbutt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // usernamebox
+            // 
+            this.usernamebox.Location = new System.Drawing.Point(301, 224);
+            this.usernamebox.Name = "usernamebox";
+            this.usernamebox.Size = new System.Drawing.Size(171, 22);
+            this.usernamebox.TabIndex = 0;
+            // 
+            // passbox
+            // 
+            this.passbox.Location = new System.Drawing.Point(301, 273);
+            this.passbox.Name = "passbox";
+            this.passbox.Size = new System.Drawing.Size(171, 22);
+            this.passbox.TabIndex = 1;
+            // 
+            // loginbutt
+            // 
+            this.loginbutt.Location = new System.Drawing.Point(301, 339);
+            this.loginbutt.Name = "loginbutt";
+            this.loginbutt.Size = new System.Drawing.Size(171, 33);
+            this.loginbutt.TabIndex = 2;
+            this.loginbutt.Text = "Login";
+            this.loginbutt.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(208, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(344, 41);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "VIKO LOGIN SCREEN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loginbutt);
+            this.Controls.Add(this.passbox);
+            this.Controls.Add(this.usernamebox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernamebox;
+        private System.Windows.Forms.TextBox passbox;
+        private System.Windows.Forms.Button loginbutt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
